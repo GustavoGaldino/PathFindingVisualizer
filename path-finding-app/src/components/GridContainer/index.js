ac81329cfe62f90a86cad58456b13b4878049391
galdinocgm <gcgm@cin.ufpe.br>
2020-09-18 18:54:15 -0300

    Added styling rules for Landing and GridContainer component

--- path-finding-app/src/components/GridContainer/index.js
+++ path-finding-app/src/components/GridContainer/index.js
@@ -4,8 +4,13 @@ import GridComponent from './GridComponent'
 import GridHeader from './GridHeader'
 
 const GridContainer = () => {
+
+    const gridContainerStyle = {
+        border: "solid 2px black"
+    }
+
     return(
-        <div className="grid-container">
+        <div className="grid-container" style={gridContainerStyle}>
             <GridHeader />
             <GridComponent
                 rows={20}
