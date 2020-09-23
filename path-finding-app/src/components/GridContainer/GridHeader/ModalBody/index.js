1d5f0fdc88f8e8d54551977ca7a010b53c3a62cc
galdinocgm <gcgm@cin.ufpe.br>
2020-09-23 20:14:38 -0300

    Added more cells informations to the CellInfo component

--- path-finding-app/src/components/GridContainer/GridHeader/ModalBody/index.js
+++ path-finding-app/src/components/GridContainer/GridHeader/ModalBody/index.js
@@ -12,6 +12,21 @@ const ModalBody = forwardRef((props,ref) => {
                 cellInfoTitle={"Visited Cell"}
                 cellInfoParagraph={"This cell was visited during the algorithm runtime."}
             />
+            <CellInfo
+                cellColor={"white"}
+                cellInfoTitle={"Walkable Cell"}
+                cellInfoParagraph={"This cell is a walkable cell, not yet visited by the algorithm."}
+            />
+            <CellInfo
+                cellColor={"green"}
+                cellInfoTitle={"Starting Cell"}
+                cellInfoParagraph={"This is the starting cell. It's the first cell considered by the algorithm."}
+            />
+            <CellInfo
+                cellColor={"red"}
+                cellInfoTitle={"Goal Cell"}
+                cellInfoParagraph={"This is the goal cell. It's the cell the algorithm is searching for."}
+            />
         </div>
     );
 });
