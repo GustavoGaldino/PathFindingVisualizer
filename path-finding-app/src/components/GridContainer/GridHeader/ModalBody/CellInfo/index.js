0de68edf041ac10f7e92c9d6f9a3620f4bd447d6
galdinocgm <gcgm@cin.ufpe.br>
2020-09-23 19:16:40 -0300

    Added new component that gives informations about the grid cells

---
+++ path-finding-app/src/components/GridContainer/GridHeader/ModalBody/CellInfo/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './styles.css'
+
+const CellInfo = (props) => {
+
+    return(
+        <div className="cell-info">
+            <div className="cell-representation" style={{backgroundColor: props.cellColor}}/>
+            <div className="cell-info-text">
+                <h1 style={{fontSize: "1.5rem"}}>{props.cellInfoTitle}</h1>
+                <p style={{fontSize: "1.2rem"}}>{props.cellInfoParagraph}</p>
+            </div>
+        </div>        
+    );
+}
+
+export default CellInfo;
--- path-finding-app/src/components/GridContainer/GridHeader/ModalBody/index.js
+++ path-finding-app/src/components/GridContainer/GridHeader/ModalBody/index.js
@@ -1,11 +1,17 @@
 import React, { forwardRef } from 'react'
 
+import CellInfo from './CellInfo'
+
 import './styles.css'
 
 const ModalBody = forwardRef((props,ref) => {
     return(
         <div ref={ref} id="modal-body">
-            <h1>Hello, World!</h1>
+            <CellInfo
+                cellColor={"cyan"}
+                cellInfoTitle={"Visited Cell"}
+                cellInfoParagraph={"This cell was visited during the algorithm runtime."}
+            />
         </div>
     );
 });
