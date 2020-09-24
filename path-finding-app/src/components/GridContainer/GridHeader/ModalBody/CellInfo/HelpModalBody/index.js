29425d8e4acea9df0244191f8a4062317c89234a
galdinocgm <gcgm@cin.ufpe.br>
2020-09-23 22:50:20 -0300

    Added gif to  demonstrate how to create wall cells

--- path-finding-app/src/components/GridContainer/GridHeader/ModalBody/CellInfo/HelpModalBody/index.js
+++ path-finding-app/src/components/GridContainer/GridHeader/ModalBody/CellInfo/HelpModalBody/index.js
@@ -2,16 +2,16 @@ import React, { forwardRef }from 'react'
 
 import './styles.css'
 
-import GridWithWallsIcon from '../../../../../../assets/images/AStar.png'
+import GridWithWallsGif from '../../../../../../assets/images/helpdemo.gif'
 
 const HelpModalBody = forwardRef((props,ref) => {
     return(
         <div ref={ref} className="help-modal-body">
             <h1>
-                To turn a walkable cell into a wall cell, you can click in the walkable cell, or
-                click and click and drag to turn multiple cells.
+                To turn a walkable cell into a wall cell, you can click in the walkable cell to modify a single cell, or
+                click and click and drag to modify multiple cells.
             </h1>
-            <img src={GridWithWallsIcon} />
+            <img src={GridWithWallsGif} />
         </div>
     );
 });
