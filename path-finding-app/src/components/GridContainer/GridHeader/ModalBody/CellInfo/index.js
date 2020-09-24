ccf7b02442de6d3cb6ddabe273885a2b0c130e3b
galdinocgm <gcgm@cin.ufpe.br>
2020-09-23 21:36:14 -0300

    Added help button to wall cell info

--- path-finding-app/src/components/GridContainer/GridHeader/ModalBody/CellInfo/index.js
+++ path-finding-app/src/components/GridContainer/GridHeader/ModalBody/CellInfo/index.js
@@ -1,17 +1,27 @@
 import React from 'react'
 
+import helpIcon from '../../../../../assets/images/help.png'
+
 import './styles.css'
 
 const CellInfo = (props) => {
-
     return(
         <div className="cell-info">
             <div className="cell-representation" style={{backgroundColor: props.cellColor}}/>
-            <div className="cell-info-text">
-                <h1 style={{fontSize: "1.2rem"}}>{props.cellInfoTitle}</h1>
-                <p style={{fontSize: "1.05rem"}}>{props.cellInfoParagraph}</p>
+            <div className="cell-info-text-container">
+                <div>
+                    <h1 style={{fontSize: "1.2rem"}}>{props.cellInfoTitle}</h1>
+                    <p style={{fontSize: "1.05rem"}}>{props.cellInfoParagraph}</p>
+                </div>
+                {props.help ?
+                    <button className="help-btn">
+                        <img src={helpIcon} />
+                    </button>
+                    :
+                    (() => {})
+                }
             </div>
-        </div>        
+        </div>    
     );
 }
 
--- path-finding-app/src/components/GridContainer/GridHeader/ModalBody/index.js
+++ path-finding-app/src/components/GridContainer/GridHeader/ModalBody/index.js
@@ -7,6 +7,7 @@ import './styles.css'
 const ModalBody = forwardRef((props,ref) => {
     return(
         <div ref={ref} id="modal-body">
+            <h1 style={{margin: ".5rem 0"}}>Description</h1>
             <CellInfo
                 cellColor={"cyan"}
                 cellInfoTitle={"Visited Cell"}
@@ -21,6 +22,7 @@ const ModalBody = forwardRef((props,ref) => {
                 cellColor={"black"}
                 cellInfoTitle={"Wall Cell"}
                 cellInfoParagraph={"This is the wall cell. This cell is not walkable."}
+                help={true}
             />
             <CellInfo
                 cellColor={"green"}
